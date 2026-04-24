00aac0a64a3a13a7b92aac3329a3cd8f7bbcc4b4
kanatad7 <hiroki.tanaka.d7@gmail.com>
2026-04-24 23:16:37 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kanatad7.github.io/garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
